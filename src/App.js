a44d16f277c7a1db4fb91b8e9f28193784d38c80
shaileh Bisht <shaileshbisht765@gmail.com>
2021-11-16 14:46:29 +0530

    import component and container in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import "./App.css";
+import React from "react";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+
+import { CTA, Brand, Navbar } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <h1>GTP-3</h1>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
